acd82eb3b9638b353e1d7023c3771383be40ba73
Gabriel <o_gabrieltorres@outlook.com>
2025-04-30 09:21:22 -0300

    De-para nos nomes das colunas.

--- backend/etl/pbx/run.py
+++ backend/etl/pbx/run.py
@@ -14,8 +14,20 @@ df_with_no_useless_cols = remove_columns(df, useless_cols)
 # Extract the name on clid and puts on a new column called 'chamador'
 df_with_no_useless_cols['chamador'] = df_with_no_useless_cols['clid'].apply(extract_name)
 
-# Remove the column clid
+# Remove the column 'clid'
 df_with_no_useless_cols = remove_columns(df_with_no_useless_cols, ['clid'])
 
-print(df_with_no_useless_cols)
+# Setting new names to the columns
+df_new_columns_names = df_with_no_useless_cols.rename(columns={
+    'calldate':'data_de_contato',
+    'callerid':'quem_ligou',
+    'dst':'quem_recebeu_ligacao',
+    'dcontext':'grupo_discagem_saida',
+    'duration':'duracao_em_segundos',
+    'disposition':'status',
+    'trunkout':'nome_do_tronco',
+    'monitor':'gravacao'
+})
+
+print(df_new_columns_names)
 # print(df_clean.info())
